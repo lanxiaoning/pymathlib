6bafecedb462f1232f66a3f422dbde887db997e4
lanxiaoning <363309464@qq.com>
2020-04-18 23:54:38 +0800

    Update trgtopi.py

--- trgtopi.py
+++ trgtopi.py
@@ -4,6 +4,7 @@
 import math
 import random
 limits=10000
+pilist=[]
 def main(testnum,samplenum):
     for i in range(testnum):
         matchcount=0
@@ -18,8 +19,16 @@ def main(testnum,samplenum):
             cos=float((a**2+b**2-c**2)/(2*a*b))
             if(-1<cos<0):
                 matchcount=matchcount+1
-        print(str(float(matchcount/samplenum)*4+2))
+        piresult=float(matchcount/samplenum)*4+2
+        pilist.append(piresult)
+        print(str(piresult))
 
 
 if(__name__=='__main__'):
-    main(20,500)
+    main(200,500)
+    count=0
+    sum=0
+    for pi in pilist:
+        count=count+1
+        sum=sum+pi
+    print('avg for pi is '+str(sum/count))
